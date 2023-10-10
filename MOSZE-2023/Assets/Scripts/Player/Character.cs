0f110a843c3a52a188e144737e8476b791bfa559
spartanguy <65070166+spartanguy@users.noreply.github.com>
2023-10-10 20:31:26 +0200

    Character+player+bullet class

---
+++ MOSZE-2023/Assets/Scripts/Player/Character.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    public Transform firepoint;
+    [SerializeField] 
+    protected float moveSpeed;
+    public Rigidbody2D rb;    
+    public GameObject bulletPrefab;
+    protected Gun gun = Guns.pistol;
+    protected bool readyToFire = true;
+    protected bool pickup = true;
+    [SerializeField] 
+    protected int health = 5;
+
+    protected void MoveCharacter(Vector2 direction)
+    {
+        rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    protected void Shoot()
+    {
+        if (gun == null) return;
+        if (!readyToFire) return;
+        readyToFire = false;
+        for (int i = 0; i < gun.GetBullets(); i++) {
+            GameObject b = Instantiate(bulletPrefab, firepoint.position + firepoint.up, transform.rotation);
+            Rigidbody2D brb = b.GetComponent<Rigidbody2D>();
+            
+            ((Bullet) b.GetComponent(typeof(Bullet))).SetDamage(gun.GetDamage());
+
+            float s = gun.GetDamage() / 2;
+            if (s > 0.5f) s = 0.5f;
+            b.transform.localScale *= 1 + s;
+
+            Vector2 dir = transform.rotation * Vector2.up;
+            Vector2 pdir = Vector2.Perpendicular(dir) * Random.Range(-gun.GetSpread(), gun.GetSpread());
+            brb.velocity = (dir + pdir) * gun.GetSpeed();
+        }
+
+        float f = gun.GetFireRate();
+        Invoke(nameof(FireCooldown), gun.GetFireRate());
+    }
+
+    protected void Aim(Vector3 target) 
+    {
+        Vector2 dir = transform.position - target;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0,0, angle + 90));
+    }
+
+    protected void FireCooldown() 
+    {
+        readyToFire = true;
+    }
+    protected void PickupCooldown() 
+    {
+        pickup = true;
+    }
+
+    public void Damage(int damage, GameObject go) {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(go);
+        }
+    }   
+}
---
+++ MOSZE-2023/Assets/Scripts/Weapons/Bullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private int damage = 1;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player" || other.tag == "Enemy")
+        {
+            Character a = (Character)other.gameObject.GetComponent(typeof(Character));
+            GameObject b = other.gameObject;
+            a.Damage(damage,b);
+            Debug.Log("hit");
+            Destroy(gameObject);
+        }
+        else{Destroy(gameObject);}
+    }
+    public void SetDamage(int d) {
+        damage = d;
+    }
+}

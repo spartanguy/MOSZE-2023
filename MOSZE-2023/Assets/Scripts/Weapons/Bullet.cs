27f4d467f2e430d7ed1ab269eaf84a67462e762c
spartanguy <65070166+spartanguy@users.noreply.github.com>
2023-11-17 11:30:51 +0100

    Bullet patch

--- MOSZE-2023/Assets/Scripts/Weapons/Bullet.cs
+++ MOSZE-2023/Assets/Scripts/Weapons/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 {
     private int damage = 1;
     private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Bullet"){return;}
         if ((gameObject.layer == LayerMask.NameToLayer("PlayerBullet") && other.tag == "Player") || (gameObject.layer == LayerMask.NameToLayer("EnemyBullet") && other.tag == "Enemy") || (other.gameObject.layer == LayerMask.NameToLayer("pickup"))) return;
         else if (other.tag == "Player" || other.tag == "Enemy")
         {

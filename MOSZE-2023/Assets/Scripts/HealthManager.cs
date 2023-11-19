117be195a158c33759fe5d49be965915563f7004
spartanguy <65070166+spartanguy@users.noreply.github.com>
2023-11-19 13:12:02 +0100

    Hearth Manager patch

--- MOSZE-2023/Assets/Scripts/HealthManager.cs
+++ MOSZE-2023/Assets/Scripts/HealthManager.cs
@@ -6,32 +6,40 @@ using UnityEngine.UI;
 public class HealthManager : MonoBehaviour
 {
     public Image[] hearts;
-    public Image[] plusHearts;
     public Sprite fullheart;
     public Sprite emptyheart;
+    public Sprite plushHeart;
 
     public int maxHp = 5;
     public int health = 5;
-    public int plusHP = 2;
+    public int plusHP = 0;
     public int maxPlusHP = 5;
 
     void FixedUpdate() {
         maxHp = Player.Instance.getMaxHp();
         health = Player.Instance.getHealth();
+        plusHP = maxHp-5;
 
-        foreach (Image kep in hearts) {
-            kep.sprite = emptyheart;
+        for (int i = 0; i < hearts.Length; i++) {
+            hearts[i].sprite = emptyheart;
         }
 
         for (int i = 0; i < health; i++){
-            hearts[i].sprite = fullheart;
+            if (i < 5)
+            {
+                hearts[i].sprite = fullheart;
+            }
+            else
+            {
+                hearts[i].sprite = plushHeart;
+            }
         }
         
         for (int j = 0; j < maxPlusHP; j++) {
             if(j<plusHP) {
-                plusHearts[j].gameObject.SetActive(true);
+                hearts[j+5].gameObject.SetActive(true);
             } else {
-                plusHearts[j].gameObject.SetActive(false);
+                hearts[j+5].gameObject.SetActive(false);
             }
         }
     }
